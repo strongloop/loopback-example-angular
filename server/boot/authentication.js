d6962bcce85d6a1c138405c5c51d8b67563eadf5
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 12:29:24 -0700

    update copyright notices and license

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
